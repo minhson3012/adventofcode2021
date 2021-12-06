ca3d6981650c6c34f6ca0eaa40dcdd90ff4330bd
Son Nguyen <sonnm@osp.vn>
2021-12-06 17:25:43 +0700

    removed even more uneccessary lines

--- day6/day6.py
+++ day6/day6.py
@@ -7,18 +7,18 @@ def main():
     for item in input:
         if int(item) in dayDict:
             dayDict[int(item)] += 1
-    challenge1Result = challenge1(dayDict)
+    challenge1Result = challenge(dayDict, 80)
     print("Challenge 1: ", challenge1Result)
-    challenge2Result = challenge2(dayDict)
+    challenge2Result = challenge(dayDict, 256)
     print("Challenge 2: ", challenge2Result)
 '''
 Looping through a massive list for 80 times is not a good idea, 
 especially when the amount of times looped can increase.
 Use a dictionary instead and count the number of times a number appears
 '''
-def challenge1(input):
+def challenge(input, times):
     currentDayDict = dict(input)
-    for i in range(0, 80):
+    for i in range(0, times):
         newDayDict = dict(currentDayDict)
         for j in range(8, -1, -1):
             if j == 8:
@@ -35,28 +35,6 @@ def challenge1(input):
     for day in currentDayDict:
         totalFish += currentDayDict[day]
     return totalFish
-
-
-def challenge2(input):
-    currentDayDict = dict(input)
-    for i in range(0, 256):
-        newDayDict = dict(currentDayDict)
-        for j in range(8, -1, -1):
-            if j == 8:
-                newDayDict[7] = currentDayDict[8]
-                newDayDict[8] = 0
-            elif j == 0:
-                newDayDict[6] += currentDayDict[0]
-                newDayDict[8] = currentDayDict[0]
-            else:
-                newDayDict[j - 1] = currentDayDict[j]
-        currentDayDict = dict(newDayDict)
-    
-    totalFish = 0
-    for day in currentDayDict:
-        totalFish += currentDayDict[day]
-    return totalFish
-
 
 def readFile(filename):
     file = open(filename, "r")
